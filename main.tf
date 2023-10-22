ad7156480b098fab862d005a84216cb87bdcfdbc
kiranthumma <kiran.thumma@ketch.com>
2023-10-22 16:25:04 -0700

    fix: kms1

--- main.tf
+++ main.tf
@@ -122,7 +122,6 @@ resource "aws_s3_bucket_public_access_block" "bucket_public_block" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
 }
 
 resource "aws_security_group" "bastion_host_security_group" {
