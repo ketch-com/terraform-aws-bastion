5c801207c92f7af5cc760336037ef914d8fd3d6c
Peter Han <git@peterhan.ca>
2024-12-16 17:49:03 -0800

    fix: remove template provider

--- versions.tf
+++ versions.tf
@@ -1,15 +1,14 @@
-
 terraform {
   required_version = ">= 0.13"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.9"
     }
     null = {
-      source = "hashicorp/null"
-    }
-    template = {
-      source = "hashicorp/template"
+      source  = "hashicorp/null"
+      version = "~> 3.2"
     }
   }
 }
